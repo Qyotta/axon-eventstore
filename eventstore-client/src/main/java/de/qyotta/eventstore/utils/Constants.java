4571f3ab5a3edd954c7add7899b76f84ce9f87c0
Frank Schmidt <fschmidt@qyotta.de>
2016-07-07 10:56:16 +0200

    try harder

--- eventstore-client/src/main/java/de/qyotta/eventstore/utils/Constants.java
+++ eventstore-client/src/main/java/de/qyotta/eventstore/utils/Constants.java
@@ -8,6 +8,8 @@ public class Constants {
    public static final String CONTENT_TYPE_JSON_EVENTS = "application/vnd.eventstore.events+json";
    public static final String ES_EVENT_TYPE_HEADER = "ES-EventType";
    public static final String ES_EVENT_ID_HEADER = "ES-EventId";
+   public static final String EMBED_PARAM = "embed";
+   public static final String TRY_HARDER = "tryharder";
 
    public static final String ACCEPT_EVENTSTORE_ATOM_JSON = "application/vnd.eventstore.atom+json";
    public static final String ACCEPT_HEADER = "Accept";
